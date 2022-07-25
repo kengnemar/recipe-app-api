5a45dd76355f0c0b00fc7cc06d1af354933c2e14
Marius KENGNE <mariuskengne@Mariuss-MacBook-Pro.local>
2022-07-25 16:28:40 -0500

    Addressed .gitignore file and .docker

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,13 +2,13 @@
 Django command to wait for the database to be available.
 """
 import time
-from psycopg2 import  OperationalError as Psycopg2Error
+from psycopg2 import  OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """ Django command to wiat for database."""
+    """ Django command to wait for database."""
 
     def handle(self, *args, **options):
        """Entrypoint for commands"""
@@ -19,7 +19,7 @@ class Command(BaseCommand):
                self.check(databases=['default'])
                db_up = True
 
-            except (Psycopg2Error, OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second....')
                 time.sleep(1)
 
