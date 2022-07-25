ae0a62d83b2ffd35be6862faf483e7cf1786aabc
Marius KENGNE <mariuskengne@Mariuss-MacBook-Pro.local>
2022-07-25 11:44:20 -0500

    Added python wait_for_db to docker-compose file

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculate functions
+"""
+
+def add(x, y):
+    """ Add x and y retung result"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x 
