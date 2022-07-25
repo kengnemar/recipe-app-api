8f6bdc27933ffe4fc490e552a2870d6b7ceea616
Marius KENGNE <mariuskengne@Mariuss-MacBook-Pro.local>
2022-07-25 17:01:14 -0500

    Added missing files after code checker results.

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,12 @@
 """
-Calculate functions
+Calculator functions
 """
 
 def add(x, y):
-    """ Add x and y retung result"""
+    """Add x and y returning result."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result."""
-    return y - x 
+    return y - x
