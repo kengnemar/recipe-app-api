be3b9ea5ce3ff5090b024744cbc4668c87d3ae48
Marius KENGNE <mariuskengne@Mariuss-MacBook-Pro.local>
2022-07-29 00:10:41 -0500

    Configured docker-compose file for deployment

---
+++ proxy/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+# env substitute with all the env variable matching name at runtime
+envsubst < /etc/nginx/default.comf.tpl > /etc/nginx/conf.d/default.conf
+nginx -g 'daemon off;'
